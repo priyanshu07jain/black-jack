24e58484a6479e27f3e06ae35da9d63188fbabe7
priyanshu jain <131845851+priyanshu07jain@users.noreply.github.com>
2023-06-26 13:49:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ amount.textContent=player.name + " "+"$"+player.chip
 let messagA =document.getElementById("messagea")
 let card=document.getElementById("add")
 let twocards= document.getElementById("show")
+let card1 = document.getElementById("back")
+let card2 = document.getElementById("front")
 function getrandomcard(){
-    let randomNumber = Math.floor(Math.random()*10)+1
+    let randomNumber = Math.floor(Math.random()*13)+1
 
     if(randomNumber === 1){
         return 11
@@ -35,10 +37,83 @@ function getrandomcard(){
 function startgame(){
      let firstcard=getrandomcard()
   let secondcard=getrandomcard()
+  
  isAlive=true
  cardes=[firstcard,secondcard]
  sum = firstcard+secondcard
+ if(firstcard===11){
+    card1.src="ace.png"
+}
+else if(firstcard===10){
+    card1.src="10sp.png"
+}
+else if(firstcard===9){
+    card1.src="9sp.png"
+}
+else if(firstcard===8){
+    card1.src="8.png"
+}
+else if(firstcard===7){
+    card1.src="7sp.png"
+}
+else if(firstcard===6){
+    card1.src="6sp.png"
+}
+else if(firstcard===5){
+    card1.src="5sp.png"
+}
+else if(firstcard===4){
+    card1.src="4sp.png"
+}
+else if(firstcard===3){
+    card1.src="3sp.png"
+}
+else if(firstcard===2){
+    card1.src="2sp.png"
+}
+
+else{
+    card1.src="back.png"
+   
+}
+
+if(secondcard===11){
+    card2.src="king.png"
+}
+else if(secondcard===10){
+    card2.src="10sp.png"
+}
+else if(secondcard===9){
+    card2.src="9sp.png"
+}
+else if(secondcard===8){
+    card2.src="8.png"
+}
+else if(secondcard===7){
+    card2.src="7sp.png"
+}
+else if(secondcard===6){
+    card2.src="6sp.png"
+}
+else if(secondcard===5){
+    card2.src="5sp.png"
+}
+else if(secondcard===4){
+    card2.src="4sp.png"
+}
+else if(secondcard===3){
+    card2.src="3sp.png"
+}
+else if(secondcard===2){
+    card2.src="2sp.png"
+}
+else{
+        
+    card2.src="back.png"
+}  
     rendergame()
+
+  
 }
 function rendergame(){
     card.textContent="sum"+":"+sum
@@ -55,7 +130,7 @@ if(sum<21){
 else if(sum === 21){
   
     hasblackjack=true
-    message="black jack"
+    message="**$$$$$**black jack**$$$$$"
 }
 else{
  
